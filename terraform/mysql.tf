2a062bdf2ec09e2c874cf22a4a008735500e0beb
Eirik Tobiassen <eirtob98@hotmail.com>
2020-11-21 22:56:06 +0100

    Name change

---
+++ terraform/mysql.tf
@@ -0,0 +1,23 @@
+terraform {	
+	required_providers {	
+          docker = {	
+          source = "terraform-providers/docker"	
+          }	
+        }	
+}	
+
+provider "docker" {}	
+
+# create db container	
+resource "docker_container" "db" {	
+  name  = "mysql"	
+  image = "mysql:5.6" //Persistence bug in more recent versions, have to use 5.6 (or different db provider)	
+  restart = "always"	
+  env = ["MYSQL_ROOT_PASSWORD=top-secret"] // This will only be set first time with persistence! Changing will have no effect unless using new mount. Insecure!
+  mounts {
+      target = "/var/lib/mysql"
+      source = "/your/persistent/path" // Change this
+      type = "bind"
+  }
+	
+}
