97670b888a2e5ed0b9d0cec3f4ab946dba0816a3
Eirik Tobiassen <eirtob98@hotmail.com>
2020-11-21 22:34:39 +0100

    Added persistence to terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,14 @@ provider "docker" {}
 
 # create db container	
 resource "docker_container" "db" {	
-  name  = "db2"	
-  image = "mysql:latest"	
+  name  = "mysql"	
+  image = "mysql:5.6" //Persistence bug in more recent versions, have to use 5.6 (or different db provider)	
   restart = "always"	
-  env = ["MYSQL_ROOT_PASSWORD=123"]	
+  env = ["MYSQL_ROOT_PASSWORD=top-secret"] // This will only be set first time with persistence! Changing will have no effect unless using new mount. Insecure!
+  mounts {
+      target = "/var/lib/mysql"
+      source = "/your/persistent/path" // Change this
+      type = "bind"
+  }
+	
 }
