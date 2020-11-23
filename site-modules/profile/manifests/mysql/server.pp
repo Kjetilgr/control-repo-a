3cdbe104e3c00fba008582cc2f3a819c6949cedb
Kjetil Grosberghaugen <kjetigr@ntnu.no>
2020-11-23 16:08:04 +0100

    Feat: add new profile for MySQL server with some custom options

---
+++ site-modules/profile/manifests/mysql/server.pp
@@ -0,0 +1,11 @@
+class profile::mysql::server {
+
+  class { '::mysql::server':
+    root_password           => 'strongpassword',
+    remove_default_accounts => true,
+    restart                 => true,
+    override_options        => $override_options
+  }
+
+}
+
